de0535b991d2e7dc9b4f6d603fb78d805e96dd0f
Thành Lã <huuthanhla@gmail.com>
2020-02-04 09:59:00 +0700

    add spinning

--- lib/main.dart
+++ lib/main.dart
@@ -18,50 +18,78 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 300));
+    animation = Tween<double>(begin: 0, end: 100).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          child: Icon(Icons.android, size: 60, color: Colors.grey[800]),
-
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
+        child: Transform.rotate(
+          angle: animation.value,
+          child: Container(
+            width: MediaQuery.of(context).size.width / 2,
+            height: MediaQuery.of(context).size.width / 2,
+            child: Icon(Icons.android, size: 60, color: Colors.grey[800]),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
                   color: Colors.grey[600],
                   offset: Offset(4.0, 4.0),
                   blurRadius: 15.0,
-                  spreadRadius: 1.0),
-              BoxShadow(
+                  spreadRadius: 1.0,
+                ),
+                BoxShadow(
                   color: Colors.white,
                   offset: Offset(-4.0, -4.0),
                   blurRadius: 15.0,
-                  spreadRadius: 1.0),
-            ],
-
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.grey[200],
-                Colors.grey[300],
-                Colors.grey[400],
-                Colors.grey[500],
+                  spreadRadius: 1.0,
+                ),
               ],
-                stops: [
-                  0.1,
-                  0.3,
-                  0.8,
-                  1
-                ]
-            )
-
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.grey[200],
+                  Colors.grey[300],
+                  Colors.grey[400],
+                  Colors.grey[500],
+                ],
+                stops: [0.1, 0.3, 0.8, 1],
+              ),
+            ),
           ),
         ),
       ),
